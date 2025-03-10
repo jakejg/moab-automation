ada723a0509154c06575961d34808f49f50ddf60
Jake <jake@Jakes-Air.etv.net>
2025-03-10 15:35:12 -0600

    updated cron

--- src/index.ts
+++ src/index.ts
@@ -24,8 +24,4 @@ async function runDailyNotification() {
   }
 }
 
-// Run every day at 9:00 AM
-cron.schedule('0 9 * * *', runDailyNotification);
-
-// Initial run for testing
 runDailyNotification();
